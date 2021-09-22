1d2815ef97d772668625e652528893b36c40a478
alfonsus.kurniawan <alfonsusanto@gmail.com>
2021-09-22 23:13:43 +0700

    chage few things

--- app/Http/Controllers/ping.php
+++ app/Http/Controllers/ping.php
@@ -12,7 +12,7 @@ class Ping extends Controller {
     public function ping(Request $request) {
         $data = PingModel::GetPing();
         $data->message = "you are connected";
-        $data->time = date('d-m-Y H:i:s', time());
+        $data->time = date('d-m-Y H:i:s', time() + (7*60*60));
 
         responsebuilder::SuccessResponseBuilder($data);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ping;
+use App\Http\Controllers\Ping;
 
 /*
 |--------------------------------------------------------------------------
