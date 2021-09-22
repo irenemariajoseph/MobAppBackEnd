bb6035ceff6a67a20aa08c1e5cf37c259ebaec57
irenemariajoseph <74368420+irenemariajoseph@users.noreply.github.com>
2021-09-23 00:15:27 +0700

    nambahfile

--- router/ping.php
+++ router/ping.php
@@ -2,9 +2,10 @@
     
     require "../entity/ping.php";
     require "../util/responsebuilder.php";
-
+    // . . -> keluar dr titik folder ini 
+    
     $data = new Ping();
-    $data->time = date('d-m-Y H:i:s', time() + (7*60*60));
+    $data->time = date('d-m-Y H:i:s', time() + (5*60*60));
     $data->message = "you are connected";
 
     BuildSuccessResponse($data, null);
